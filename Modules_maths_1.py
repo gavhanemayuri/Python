8a1a095c318461c7c15aaa02d32cc86f41d0d295
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-01-10 22:57:14 +0530

    Add files via upload

---
+++ Modules_maths_1.py
@@ -0,0 +1,13 @@
+#import modules    
+import maths      # maths another file .py
+
+print(maths.add(5,3))
+print(maths.sub(10,4))
+
+
+#built-in modules
+import math
+
+print(math.sqrt(16))
+print(math.factorial(5))
+
