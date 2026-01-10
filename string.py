dc031833cd54da5e855693c04773726c9b8b6cae
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-01-10 22:55:27 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,51 @@
+s = "Hello Python"
+print(s)             #string operation 
+
+stack=[1,2,3,4,5]
+stack.append(6)
+print (stack)               #stack
+
+stack_1=[1,2,3,4]
+stack_1.append(5)                   #add
+stack_1.append(6)
+stack_1.pop()                   #delete 
+print(stack_1)             
+
+
+
+queue=[12,13,14,15,16]
+queue.append(17)
+queue.append(18)
+queue.pop()
+print(queue)
+
+queue=[]
+queue.append(17)
+queue.append(18)
+queue.pop()
+print(queue)                            #queue   ,append ,pop
+
+
+
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print("Queue after enqueue:", queue)
+
+# Dequeue
+queue.pop(0)
+print("Queue after dequeue:", queue)
+
+# Front element
+print("Front element:", queue[0])
+
+
+
+subject=["maths","science","marathi","english","history","computer"]
+print(subject)
+
+
+
