49a7d7d1b314afef909e9e7981d409c298fde1fb
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-01-28 23:37:10 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,27 @@
+for i in range(1,10):
+    print(i)                                      #for loop
+
+
+
+
+fruits=["apple","banana","orange"]
+
+for m in fruits:                            #fruits is list
+    print(m)                          # m is    
+
+
+
+
+# a=1
+# while a <= 5:
+# print(a)
+# a += 1
+
+
+for i in range(1,6):
+    if i % 2== 0:
+        print(i ,"is even")
+    else:                                  #even and odd
+        print(i,"is odd")
+
+        
